3bf7fa125d5f4b5d3a8ce25f3629dd11fc8e1a5b
jugghead <arghyathengal@gmail.com>
2016-09-21 20:23:28 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/hello', function (req, res) {
+  res.send('coming up soon');
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
