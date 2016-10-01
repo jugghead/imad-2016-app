eba041185cacfbd3460449c2d3407311a61d7d2d
jugghead <arghyathengal@gmail.com>
2016-10-02 00:41:05 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,6 +3,13 @@ console.log('Loaded!');
 //madi animation
 
 var img = document.getElementById('madi');
+
+marginleft=0;
+
+function moveright() {
+    marginleft=marginleft+10;
+    imgstyle.marginLeft= marginleft + 'px';
+}
 img.onclick = function() {
-    img.style.marginLeft='100px';
+    var interval = setInterval(moveright, 100);
 };
