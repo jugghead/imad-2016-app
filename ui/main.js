45ed5dfddf381910d442386c755cd278cc2805d8
jugghead <arghyathengal@gmail.com>
2016-10-02 00:41:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -8,7 +8,7 @@ marginleft=0;
 
 function moveright() {
     marginleft=marginleft+10;
-    imgstyle.marginLeft= marginleft + 'px';
+    img.style.marginLeft= marginleft + 'px';
 }
 img.onclick = function() {
     var interval = setInterval(moveright, 100);
